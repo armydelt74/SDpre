52172b77f3bc307976dc82f00aa48da01407fb17
armydelt74 <kgneiting@gmail.com>
2019-10-30 09:59:19 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "Kent is determined to learn how to code." );
+    System.out.println( "Today's date is October 30th, 2019" );
+  }
+}
