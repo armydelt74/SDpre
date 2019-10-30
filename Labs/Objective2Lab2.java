84a07370aba5794577a79cb4e4bfa31c8d4180a6
armydelt74 <kgneiting@gmail.com>
2019-10-30 13:02:17 -0600

    Labs/Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kent Gneiting";
+    int age = 35;
+    String state = "Idaho";
+
+  System.out.println (name);
+  System.out.println (age);
+  System.out.println (state);
+  }
+}
