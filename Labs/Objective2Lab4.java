c2da8257197edaba5db183db3c7a480441fc7985
armydelt74 <kgneiting@gmail.com>
2019-10-30 14:01:06 -0600

    Obj2 Lab 4 and 5

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,16 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, sum, average;
+
+    num1 = 27.5;
+    num2 = 66.6;
+    num3 = 4.4;
+    sum = (num1 + num2 + num3);
+    average = (sum/3);
+
+    System.out.println( "1st number" + " " + num1);
+        System.out.println( "2nd number" + " " + num2);
+            System.out.println( "3rd number" + " " + num3);
+                System.out.println( "The average" + " " + average);
+  }
+}
---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,13 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    double num = Math.sqrt(10 * 10);    //num => 3
+   double result = Math.sqrt(164); //result => 2 + 3 => 5
+
+   side1 = 10;
+   side2 = 8;
+   hypotenuse = result;
+
+   System.out.println("The hypotenuse of a triangle with sides 10 and 8 is" + " " + hypotenuse);
+  }
+}
