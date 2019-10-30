9e0e3be350a012496f79261d69164943bd25fbb7
armydelt74 <kgneiting@gmail.com>
2019-10-30 16:15:01 -0600

    Obj3 Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    String name = "";
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What's your name?");
+    name = input.next();
+
+    System.out.println(name);
+
+    /*
+      Your solution goes here
+    */
+
+    input.close();
+  }
+}
