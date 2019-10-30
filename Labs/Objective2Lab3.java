e862df140da44b243707285cda7b1e2f976e6985
armydelt74 <kgneiting@gmail.com>
2019-10-30 13:22:25 -0600

    Obj2 Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 = "beer";
+    String food2 = "sushi";
+    String food3 = "spam";
+
+  System.out.println (food1 + food2 + food3);
+    System.out.println (food1);
+      System.out.println (food2);
+        System.out.println (food3);
+
+  }
+}
